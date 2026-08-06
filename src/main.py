cf715399af15367bc7a271c6f02504c00a1ecd27
brotcon26 <brotcon26@gmail.com>
2026-08-06 12:18:47 -0700

    tested in ground test mode, still looks good

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ from machine import Pin, PWM
 
 import gc
 
-GroundTest = False #sets whether to ground test or not. If true, this replaces real data with replayed data from a file
+GroundTest = True #sets whether to ground test or not. If true, this replaces real data with replayed data from a file
 Slowmode = False #If we are in ground test mode, this can also be enabled. This delays 10 seconds after each loop and prints some of the data
 slowmodeDelay = 0.2 #Delay time for slowmode in seconds
 
