5a8ca91d1845d1a416e703409ddf7028eed5ba18
brotcon26 <brotcon26@gmail.com>
2026-08-07 09:46:24 -0700

    Testing for merge

--- src/main.py
+++ src/main.py
@@ -16,8 +16,8 @@ from machine import Pin, PWM
 import gc
 
 GroundTest = True #sets whether to ground test or not. If true, this replaces real data with replayed data from a file
-Slowmode = False #If we are in ground test mode, this can also be enabled. This delays 10 seconds after each loop and prints some of the data
-slowmodeDelay = 0.2 #Delay time for slowmode in seconds
+Slowmode = True #If we are in ground test mode, this can also be enabled. This delays 10 seconds after each loop and prints some of the data
+slowmodeDelay = 0 #Delay time for slowmode in seconds
 
 def getAltitude(pressure):
     return (145366.45 * (1.0 - pow(pressure / 1013.25, 0.190284))) # returns altitude in feet
